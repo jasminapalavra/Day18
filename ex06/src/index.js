c565f77277305e760d22aa1ec22eec241f75c37c
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-26 10:50:08 +0100

    Added ex06 src and index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+
+let newRock = new Map();
+
+newRock.set("artist", "The Rolling Stones");
+newRock.set("song", "Angie");
+newRock.set("album", "Goats Head Soup");
+newRock.set("singer", "Mick Jagger");
+
+
+
+
+console.log(newRock);
+
+
+module.exports = { rockStar, newRock }
+
+
+
+
