1b2344cbfe07448be736ebc01be0c0362b7126a8
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-26 10:46:19 +0100

    Added ex05 src and index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,31 @@
+
+let shoppingList = new Map([
+arrKey = [
+    groceries: 'Banana',
+    groceries: 'Pineapple',
+    groceries: 'Pear',
+    groceries: 'Carrot',
+    groceries: 'Apple',
+]
+
+
+
+arrValue = ([
+    amount: 3,
+    amount: 5,
+    amount: 2,
+    amount: 10,
+    amount: 1.5,
+]
+
+
+    arrKey.forEach((key) => {
+        console.log("groceries: ", key);
+      });
+
+
+module.exports = { shoppingList, arrKey, arrValue };
+
+
+
+
